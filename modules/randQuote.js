e928345acbd9459154e5ed2745eaf79d957b8065
Julian <julian.h.bonilla@gmail.com>
2023-02-06 10:57:14 -0800

    found placeholder still in quote 3

--- modules/randQuote.js
+++ modules/randQuote.js
@@ -2,7 +2,7 @@ export function randomQuote() {
   let quotes = [
     "If you no longer go for a gap that exists, you're no longer a racing driver.",
     "I have no idols. I admire work, dedication, and competence.",
-    "[REDACTED]",
+    "Being second is to be the first of the ones who lose.",
   ];
   let quoteNum = Math.floor(Math.random() * quotes.length);
   console.log(quotes[quoteNum]);
